9398911797192700d5cac63c0a78ad9c7a338324
李东华 <lidonghua@jd.com>
2017-08-11 14:26:06 +0800

    vue-jmv data

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,22 +1,15 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    {{#router}}
     <router-view></router-view>
-    {{else}}
-    <hello></hello>
-    {{/router}}
   </div>
 </template>
 
 <script>
   export default {
-    name: 'jmv'
+    name: 'projectName'
   }
 </script>
 
-<style>
-  * {
-    box-sizing: border-box;
-  }
+<style rel="stylesheet/scss" lang="scss">
+  @import "assets/css/_common.scss";
 </style>
---
+++ template/src/views/Index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="page-index">
+        <img src="../assets/images/logo.png" class="logo">
+        <h1>\{{ title }}</h1>
+        <p>\{{ text }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'index',
+        data() {
+            return {
+                title: 'Welcome to use jmv to build your Vue.js App',
+                text: "It's a webpack&vue&vuex&vue-router project.",
+            }
+        }
+    }
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+    .page-index {
+        overflow: hidden;
+        text-align: center;
+        .logo {
+            margin-top: 30px;
+        }
+
+        h1 {
+            font-size: 18px;
+        }
+    }
+</style>
